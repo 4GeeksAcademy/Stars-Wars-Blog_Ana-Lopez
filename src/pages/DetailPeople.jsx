b2b3d37c173b16b6b2cb9ed13d181d4bea463fd5
Ana Lopez <avantadesignlab@gmail.com>
2026-04-11 20:02:07 +0000

    feat: create character detail component with API integration, dynamic routing and loading state

--- src/pages/DetailPeople.jsx
+++ src/pages/DetailPeople.jsx
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom"
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-import React, { useEffect } from "react";
+import React, { useEffect, useState} from "react";
 
 
 export const DetailPeople = () => {
@@ -9,37 +10,49 @@ export const DetailPeople = () => {
 
     const { uid } = useParams()
 
+    const [personaje, setPersonaje] = React.useState(null)
+
     function detallePersonaje() {
         fetch("https://www.swapi.tech/api/people/" + uid)
             .then(res => res.json())
-            .then(data => console.log(data.result.properties))
+            .then(data => setPersonaje(data.result.properties))
             .catch(err => console.error(err))
     }
 
-useEffect(() => {
+    useEffect(() => {
         detallePersonaje()
 
-    },[uid])
+    }, [uid])
 
+    if (!personaje) return <p>Loading...</p>
 
 
     return (
         <div className="container">
 
             <div className="card" style={{ width: "18rem;" }}>
-                <img src="..." className="card-img-top" alt="..." />
+                <img
+                 src={`https://raw.githubusercontent.com/breatheco-de/swapi-images/master/public/images/people/${uid}.jpg`} 
+                    className="card-img-top" 
+                    alt={personaje.name}
+                    style={{ height: "300px", objectFit: "cover" }}
+                />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <h5 className="card-title">{personaje.name}</h5>
+                    <p className="card-text">Gender: {personaje.gender}</p>
                 </div>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item">An item</li>
-                    <li className="list-group-item">A second item</li>
-                    <li className="list-group-item">A third item</li>
+                    <li className="list-group-item">Height: {personaje.height}</li>
+                    <li className="list-group-item">Mass: {personaje.mass}</li>
+                    <li className="list-group-item">Hair color: {personaje.hair_color}</li>
+                    <li className="list-group-item">Eye color: {personaje.eye_color}</li>
                 </ul>
 
-            </div>
 
+            </div>
+            <Link to="/">
+                <button className="btn btn-primary">Back home</button>
+            </Link>
         </div>
     );
 }; 
